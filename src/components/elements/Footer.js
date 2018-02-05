5bbcd4486504db2b88840b861c2dc06075bfb229
Deniz Skantz <denizskantz@hotmail.com>
2018-02-05 13:50:32 -0500

    some additional footer

--- src/components/elements/Footer.js
+++ src/components/elements/Footer.js
@@ -8,7 +8,8 @@ export const Footer = () => {
         <div className="nav">
           <ul className='cf'>
             <li className='cf'>
-                © Deniz Skantz 2018 | <a href="mailto:denizskantz@hotmail.com">Contact me</a>
+                © Deniz Skantz 2018 | <a href="mailto:denizskantz@hotmail.com">Contact me</a> | <a href ="https://www.linkedin.com/in/denizskantz/"> LinkedIn </a>
+                | <a href ="https://github.com/DenizSka">GitHub</a>
             </li>
           </ul>
         </div>
